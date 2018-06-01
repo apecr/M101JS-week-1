dcf081511b7d93299f3d562e89b4a1accd751a39
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-06-01 12:25:51 +0200

    feat() Modifications for the routes
    Important mention that the errorHandler must be put after the url handlers
    
    [FA-77]

--- handling-in-express/app.js
+++ handling-in-express/app.js
@@ -1,33 +1,37 @@
 const express = require('express');
 const consolidate = require('consolidate');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.engine('html', consolidate.swig);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.bodyParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const errorHandler = (error, req, res, next) => {
+const personalErrorHanlder = function(error, req, res, next) {
+  if (res.headersSent) {
+    return next(error);
+  }
   console.error(error.message);
   console.error(error.stack);
   res.status(500);
   res.render('error_template', {error});
 };
 
-app.use(errorHandler);
 
-app.get('/:name', (req, res, next) => {
-  const name = req.params.name;
-  const getVar1 = req.query.getvar1 || 'not set yet';
-  const getVar2 = req.query.getvar2 || 'not set yet';
-  res.render('hello', {
-    name,
-    getVar1,
-    getVar2
-  });
-});
+app.get('/', (req, res, next) => res.render('fruitPicker', {
+  fruits: ['apple', 'orange', 'banana', 'peach']
+}));
 
+const postFavoriteFruit = (req, res, next) =>
+  req.body.fruit ?
+    res.send(`Your favourite fruit is ${req.body.fruit}`) :
+    next(Error('Select a fruit'));
+
+app.post('/favorite_fruit', postFavoriteFruit);
+
+app.use(personalErrorHanlder);
 app.listen(3000);
 console.log('Express server listening on port 3000');
